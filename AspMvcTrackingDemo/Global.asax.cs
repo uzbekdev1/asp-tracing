99f807063a4e447939388b2d3457d024b9092a96
Elyor Latipov <elyor@outlook.com>
2021-09-27 19:01:26 +0500

    first commit

---
+++ AspMvcTrackingDemo/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using AspMvcTrackingLib;
+using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace AspMvcTrackingDemo
+{
+    public class MvcApplication : HttpApplication
+    {
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            //if (User.Identity.IsAuthenticated)
+            Context.WriteAccessLog("12345");
+        }
+
+    }
+}
---
+++ AspMvcTrackingLib/TrackingManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AspMvcTrackingLib
+{
+    public static class TrackingManager
+    {
+         
+        private static string GetAuditLog(HttpContext context)
+        {
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("-------------" + DateTime.Now + "------------------");
+
+            var ip = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                ip = context.Request.ServerVariables["REMOTE_ADDR"];
+            }
+            sb.AppendLine("Remote IP:" + ip);
+            sb.AppendLine("Request Method:" + context.Request.HttpMethod);
+            sb.AppendLine("Request URL:" + context.Request.Url);
+            sb.AppendLine("User agent:" + context.Request.UserAgent);
+            sb.AppendLine("Headers:");
+            var heades = context.Request.Headers;
+            for (var i = 0; i < heades.Count; i++)
+            {
+                sb.AppendLine("     " + heades.GetKey(i) + ":" + heades.Get(i));
+            }
+
+            sb.AppendLine("Post data:");
+            var stream = context.Request.InputStream;
+            if (stream != null && stream.Length > 0)
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                var reader = new StreamReader(stream);
+                sb.AppendLine(reader.ReadToEnd());
+            }
+
+            sb.AppendLine("---------------------------------------------------");
+
+            return sb.ToString();
+        }
+
+        public static void WriteAccessLog(this HttpContext context, string user)
+        {
+
+            var date = DateTime.Now;
+            var root = context.Server.MapPath("~/");
+            var dir = Path.Combine(root, date.Year.ToString(), date.Month.ToString(), date.Day.ToString());
+            
+            Directory.CreateDirectory(dir);
+
+            var path = Path.Combine(dir, string.Format("access-{0}.log", user));
+            var logs = GetAuditLog(context);
+
+            Task.Factory.StartNew(() =>
+            {
+                File.AppendAllText(path, logs, Encoding.UTF8);
+            });
+        }
+
+    }
+}
